b9220f7bb38a83fd53863d34cad153a31fb362d7
jucastano <me@jucastano.com>
2020-11-27 16:06:47 -0500

    Deletion

--- vp/download.php
+++
@@ -1,9 +0,0 @@
-<?php
-$filename = "members.csv";
-
-header("Content-Length: " . filesize($filename));
-header('Content-Type: application/octet-stream');
-header('Content-Disposition: attachment; filename=members.csv');
-
-readfile($filename);
-?>
--- vp/includes/title.inc.php
+++
@@ -1,5 +0,0 @@
-<?php
-// This life is used to dynamically set the title of each web page that uses it.
-// Get the name of the file and strip the .php extension
-$title = ucfirst(basename($_SERVER['SCRIPT_NAME'],'.php'));
-?>
